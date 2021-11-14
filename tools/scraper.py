567871914be5b7925617ad304cd7b17f4bd617b9
meganndare <megann.dare@gmail.com>
2021-11-14 14:35:44 +0100

    extract urls from xlsx

--- tools/scraper.py
+++ tools/scraper.py
@@ -2,17 +2,41 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import openpyxl
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://americanliterature.com/author/eleanor-hallowell-abbott/short-story/peace-on-earth-good-will-to-dogs")
-content = driver.page_source
 
-soup = BeautifulSoup(content)
+def extract_links_from_excel_file():
+    short_stories_excel = openpyxl.load_workbook('4000-Stories-with-sentiment-analysis.xlsx', data_only=True)
+    print('loaded .xlsx workbook')
+    sheet = short_stories_excel['Sheet1']
 
-story_text = ''
-story = soup.find("div", {"class":"jumbotron", "itemtype":"https://schema.org/ShortStory"}).findAll('p')
-print(len(story))
+    urls = []
 
-for p in story:
-    story_text += ' ' + ''.join(p.findAll(text = True))
-print(story_text)
+    for cell in sheet['B']:
+        print(cell.value)
+        if cell.value != "url":
+            urls.append(cell.value)
+    
+    return urls
+
+
+
+def scrape_story(url: str):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
+    content = driver.page_source
+
+    soup = BeautifulSoup(content)
+
+    story_text = ''
+    story = soup.find("div", {"class":"jumbotron", "itemtype":"https://schema.org/ShortStory"}).findAll('p')
+    print(len(story))
+
+    for p in story:
+        story_text += ' ' + ''.join(p.findAll(text = True))
+    print(story_text)
+
+urls = extract_links_from_excel_file()
+
+for url in urls:
+    scrape_story(url)
