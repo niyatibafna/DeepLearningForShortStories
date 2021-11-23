5a4bf8c024c4ef263bddcbc47121fee293ad3ffe
linus <pjlintw@gmail.com>
2021-11-23 15:23:23 +0100

    add sim

--- utils/similarity.py
+++ utils/similarity.py
@@ -1,8 +1,8 @@
+
+import torch.nn as nn
 import numpy as np
 from numpy.linalg import norm
 
 def compute_cosine_similarity(a, b):
     """Compute cosine similarity."""
     return np.dot(a,b) / (norm(a)*norm(b))
-
-    
