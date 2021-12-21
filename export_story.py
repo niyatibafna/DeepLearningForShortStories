3151a62a71fb9f2d985183ba07949f88dd37e079
Pin-Jie Lin <pinjie@jones-6.coli.uni-saarland.de>
2021-12-21 11:44:31 +0100

    export story file with bert tokenizer

--- export_story.py
+++ export_story.py
@@ -4,14 +4,17 @@ import re
 import argparse
 import pandas as pd
 
+from tqdm import tqdm
 from utils.utils import write_to_file
+from transformers import BertTokenizer
+
 
 def get_args():
     """Parse arguments."""
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--data", type=str, default="4000-Stories-with-sentiment-analysis.xlsx")
-    parser.add_argument("--output_dir", type=str, default="")
-    
+    parser.add_argument("--data", type=str, default="data/source/4000_content_only.xlsx")
+    parser.add_argument("--output_dir", type=str, default="data/bert_tokenizer")
+    parser.add_argument("--tokenizer_name", type=str, default=None)
     return parser.parse_args()
     
 
@@ -19,21 +22,32 @@ def main():
     # Arguments
     args = get_args()
 
-    data_dir = "data/raw/"
+    data_dir = args.output_dir
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    df = pd.read_excel(args.data)
+    # Load tokenizer
+    tokenizer_name = args.tokenizer_name
+    tokenizer = BertTokenizer.from_pretrained(tokenizer_name) if tokenizer_name else None
 
-    for row in df.itertuples(index=True):    
+    print("Loading dataframe")
+    df = pd.read_excel(args.data)
+    
+    num_row = df.shape[0]
+    for row in tqdm(df.itertuples(index=True), total=num_row):    
         story_no = str(row.Index)
-        story = row.story
+        
+        if tokenizer_name:
+            story = " ".join(tokenizer.tokenize(row.story))
+        else:
+            story = row.story
+
         # Do lowercasing and remove all except 0..9, a-z and A..Z.
         title = re.sub(r"\s+", " ", row.title.lower()).replace(" ", "_")
         title = re.sub('[^A-Za-z0-9_]+', '', title)
         title = re.sub(r"_+", "_", title)
 
-        output_file = os.path.join(args.output_dir, f"{data_dir}/{story_no}_{title}.txt")
+        output_file = os.path.join(args.output_dir, f"{story_no}_{title}.txt")
         write_to_file(output_file, story)
 
 if __name__ == "__main__":
